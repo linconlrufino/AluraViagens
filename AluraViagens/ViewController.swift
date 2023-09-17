269b96318267910418497e01c6115fa311ac2ee5
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-09-17 19:42:41 -0300

    UITableViewDataSource

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viagensTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        viagensTableView.dataSource = self
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "Viagem \(indexPath.row)"
+        
+        return cell
+    }
 }
 
