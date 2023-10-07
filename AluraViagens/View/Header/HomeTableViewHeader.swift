cbce50208b6d303c867e7ec5f5d86002893ef0fe
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-10-07 20:31:07 -0300

    #Arredondando view

--- AluraViagens/View/Header/HomeTableViewHeader.swift
+++ AluraViagens/View/Header/HomeTableViewHeader.swift
@@ -13,9 +13,34 @@ class HomeTableViewHeader: UIView {
     
     @IBOutlet var tituloLabel: UILabel!
     @IBOutlet var headerView: UIView!
+    @IBOutlet var bannerView: UIView!
     @IBOutlet var bannerImageView: UIImageView!
     
     func configuraView () {
         headerView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 85
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
 }
+
+/*
+ 
+ layerMinXMinYCorner ==> left top corner
+ layerMaxXMinYCorner ==> right top corner
+ layerMinXMaxYCorner ==> left bottom corner
+ layerMaxXMaxYCorner ==> right bottom corner
+ 
+   Y | 0
+     |
+     |
+X    |
+-----|-------->
+0    |
+     |
+     |
+     v
+ 
+ */
