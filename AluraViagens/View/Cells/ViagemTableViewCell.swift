2053fe51b75559d0b121c6b3f08ef031e02a26f2
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-10-08 23:18:51 -0300

    Criando uma nova célula

---
+++ AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Linconl Rufino on 08/10/23.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
