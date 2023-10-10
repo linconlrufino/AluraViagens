f7ed30c1f39a98782af17007c6bda2b0779d79a1
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-10-09 22:24:26 -0300

    Utilizando mock para listar as viagens

---
+++ AluraViagens/ViewModel/ViagemViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ViagemViewModel.swift
+//  AluraViagens
+//
+//  Created by Linconl Rufino on 09/10/23.
+//
+
+import Foundation
+
+enum ViagemViewModelType: String {
+    case destaques
+    case ofertas
+    case internacionais
+}
+
+protocol ViagemViewModel {
+    var tituloSessao: String { get }
+    var tipo: ViagemViewModelType { get }
+    var viagens: [Viagem] { get set }
+    var numeroDeLinhas: Int { get }
+}
