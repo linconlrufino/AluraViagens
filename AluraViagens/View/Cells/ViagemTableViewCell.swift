117eaa4b997dbf67463b8d68360a3239e1ce89ad
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-10-09 22:51:33 -0300

    Configurando célula da TableView

--- AluraViagens/View/Cells/ViagemTableViewCell.swift
+++ AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -19,4 +19,26 @@ class ViagemTableViewCell: UITableViewCell {
     @IBOutlet weak var precoSemDescontoLabel: UILabel!
     @IBOutlet weak var precoViagemLabel: UILabel!
     @IBOutlet weak var statusCancelamentoViagemLabel: UILabel!
+    
+    func configuraCelula(_ viagem: Viagem?){
+        viagemImage.image = UIImage(named: viagem?.asset ?? "")
+        tituloViagemLabel.text = viagem?.titulo
+        subtituloViagemLabel.text = viagem?.subtitulo
+        precoViagemLabel.text = "R$ \(viagem?.preco ?? 0)"
+
+        let atributoString: NSMutableAttributedString = NSMutableAttributedString(string: "R$\(viagem?.precoSemDesconto ?? 0)")
+        
+        atributoString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: 1, range: NSMakeRange(0,atributoString.length))
+        
+        precoSemDescontoLabel.attributedText = atributoString
+        
+        if let numeroDeDias = viagem?.diaria, let numeroDeHospedes = viagem?.hospedes {
+            let diarias = numeroDeDias == 1 ? "Díaria" : "Diárias"
+            let hospedes = numeroDeHospedes == 1 ? "Pessoa" : "Pessoas"
+                
+            diariaViagemLabel.text = "\(numeroDeDias) \(diarias) - \(numeroDeHospedes) \(hospedes)"
+        }
+        
+        
+    }
 }
